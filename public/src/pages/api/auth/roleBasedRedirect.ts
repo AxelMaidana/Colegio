a63b569355444507b4436b27f65d4c71adf28113
Axel Maidana <maidana.axel2003@gmail.com>
2024-10-14 13:19:19 -0300

    asd

--- public/src/pages/api/auth/roleBasedRedirect.ts
+++ public/src/pages/api/auth/roleBasedRedirect.ts
@@ -4,37 +4,36 @@ import { app } from "../../../firebase/server";
 
 // Definir los tipos básicos para los parámetros
 export async function checkUserRole(Astro: any, allowedRoles: string[]) {
-    console.log("Starting checkUserRole");
-
     const auth = getAuth(app);
     const db = getFirestore(app);
-    
+
     // Verificación de la existencia de la cookie de sesión:
     const sessionCookie = Astro.cookies.get("__session")?.value;
     if (!sessionCookie) {
-        console.log("No session cookie found.");
-        return null; // Retorna null en lugar de redirigir
+        console.log("No session cookie found, redirecting to signin");
+        return Astro.redirect("/signin"); 
     }
 
     try {
-        console.log("Verifying session cookie");
+        // Verificación de la cookie de sesión y recuperación de datos:
         const decodedClaims = await auth.verifySessionCookie(sessionCookie);
-        console.log("Decoded claims:", decodedClaims); // Log de claims decodificados
-
         const userDoc = await db.collection("users").doc(decodedClaims.uid).get();
         const userData = userDoc.data();
-        console.log("User data retrieved:", userData); // Log de datos del usuario
 
-        // Verificación de los datos del usuario y redirección si no se cumplen los requisitos:
+        // Verificación de los datos del usuario y redirección a la página de inicio de sesión si no se cumplen los requisitos:
         if (!userData || !allowedRoles.includes(userData.role)) {
-            console.log("User role not allowed or user data missing, redirecting to signin");
-            return null; // Retorna null si no cumple los requisitos
+            // Destruir la cookie antes de redirigir
+            Astro.cookies.delete("__session"); // Eliminar la cookie de sesión
+            return Astro.redirect("/signin");
         }
 
         return userData;
 
     } catch (error) {
         console.error("Error verifying session cookie or fetching user data:", error);
-        return null; // Retorna null en caso de error
+        
+        // Destruir la cookie en caso de error
+        Astro.cookies.delete("__session"); // Eliminar la cookie de sesión
+        return Astro.redirect("/signin");
     }
 }
